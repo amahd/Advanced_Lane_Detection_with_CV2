c63f123e4c2e946163156888490ff45916bcee39
amahd <aneeq.sdc@gmail.com>
2017-04-27 15:44:19 +0200

    Code clean up for final video and pipeline

--- main.py
+++ main.py
@@ -10,150 +10,153 @@ email: aneeq.sdc@gmail.com
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+
 plt.close('all')
 #importing pipeline
-from Pipeline_helper import Pipeline_helper,Line_Stats
-#from Line_Stats import Line_Stats
+from Pipeline_helper import Pipeline_helper,Line_Sanity, fitCoeff
 
 import convol as con
 
 from moviepy.editor import VideoFileClip
 
+""" Directories for calibration and test images """
 
-calib_image_dir = "camera_cal/"
-test_image_dir = "test_images/"
+calib_image_dir = "camera_cal/"     #directory where clibration images are places
+test_image_dir = "test_images/"     # dir for test images
 
-NX = 9
-NY = 6
+""" Image calibration data """
+NX = 9          # no of inner points on xaxis 
+NY = 6          # No of inner points on y axis
 
-window_width = 50 
-window_height = 80 # Break image into 9 vertical layers since image height is 720
-margin = 100 # How much to slide left and right for searching
+""" Convolution window variables """
+window_width = 50    # Convolution window width
+window_height = 80   # Break image into 9 vertical layers since image height is 720
+margin = 100          # How much to slide left and right for searching
 
+# font to sue to write on top of video images
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-VALX = 40
-VALY = 40
-VALM = 50
-s = 3.0
+""" Threshold for edge and color detection """
 
-absx = [5, [VALX,VALX*s]]
-absy = [5, [VALY,VALY*s]]
-mag = [5, [VALM,VALM*s]]
-abs_dir = [25, [0.9,1.1]]
-thresh_col_s = [100,255]
-thresh_col_l = [220,255]
+VALX = 40       # minimum threshold for Sobel on x axis
+VALY = 40       # minimum threshold for Sobel on y axis
+VALM = 50       # # minimum threshold for magnitude based threshold search
+s = 3.0         # range for maximum threshold
 
-line = Line_Stats()
+absx = [5, [VALX,VALX*s]]   # Sobel size and thresholds for x axis derivative
+absy = [5, [VALY,VALY*s]]   # Sobel size and thresholds for y axis derivative
+mag = [5, [VALM,VALM*s]]    # Sobel size and thresholds for magnitude thresh detection
+abs_dir = [25, [0.9,1.1]]   # Sobel size and thresholds for directional detection
+
+thresh_col_s = [100,255]    # Color threshold for s channel
+
+""" global variables to store image count and radius values """
 indx = 0
-
 radius = [0,0]
-#def pipeline(image):
-#    
-#    
-#    imge =  np.array(image)
-#    
-#    # Caluclate graients
-#    a,b,c,d = pipe.calcGradients(imge, absx, absy, mag, abs_dir )
-#    
-#    #perform filtering using color threshold
-#    s_binary = pipe.colorThr(imge,thresh_col)
-#    
-#    combined_binary = pipe.getBinaryImg( a,b,c,d, s_binary )
-#    
-#    warped,img = pipe.getWarpedImg(imge, combined_binary)    
-#    
-#    leftx , lefty , rightx, righty= con.find_window_centroids(warped, window_width, window_height, margin)
-#
-#    left_fitx, right_fitx, ploty = pipe.fitLine(warped,leftx, lefty,rightx,righty)
-#    
-#    
-#    l,r = pipe.getRadiusCurve(np.array(left_fitx), np.array(right_fitx),ploty)
-#    
-#    radius = float("{0:.2f}".format((l+r)/2.0))
-#    line_stat.collectRadius(radius) 
-#    string = "Radius of Curvature is = "+str(radius)+" (m)"
-#    
-#    n_img = pipe.warpToColor(imge, warped,left_fitx, right_fitx, ploty)
-#    cv2.putText(n_img,string,(300,150), font, 1,(255,255,255),2)
-#    
-##    return n_img, warped, left_fitx, right_fitx, ploty,img
-#    return n_img
+
 
 def pipeline(image):
+    """
+    Main pipeline for handling video images
+    
+    Input:
+        image: vidoe image
+    Ouput:
+        n_img: processed image with lane lines
+    """
+    
     centre_x = []
     centre_y = []
-    imge =  np.array(image)
-    global indx
+    temp =  np.array(image)
+    global indx,mat,coeff
+
+    #undistort image
+    imge = pipe.cameraDistremove( temp, mat,coeff,  True)
     
-    # Caluclate graients
+    # Caluclate all 4 gradients based threhsold detection
     a,b,c,d = pipe.calcGradients(imge, absx, absy, mag, abs_dir )
     
     #perform filtering using color threshold
     s_binary = pipe.colorThr_s(imge,thresh_col_s)
-#    s_binary = pipe.colorThr(imge,thresh_col_s,thresh_col_l)
-#    
+
+    # get final binary image   
     combined_binary = pipe.getBinaryImg( a,b,c,d, s_binary )
     
+    # get the warped version of the image
     warped,img = pipe.getWarpedImg(imge, combined_binary)   
 
-    centre_x, centre_y = con.find_window_centroids(warped, window_width, window_height, margin)
+    # get initial lane points using convolution on warped image
+    centre_x, centre_y = con.find_window_centroids(warped, window_width, window_height)
     
+    # get all points for left and right lane
     leftx , lefty , rightx, righty = con.find_all_centroids(warped, centre_x, centre_y,
                                                             window_width, window_height, margin)
     
-#    if (line.checkLaneWidth(leftx,rightx,righty)):   # Check on lane width
     
-       
+    # Sanity check if the convolved points are reasonable, returns True/False 
     val = line.checkLaneWidth(leftx,lefty,rightx,righty)    
         
-
+    # If the queue is filled with all the correct points so far,
+    # use thee mean coefficients  
     if (len(line.Leftx) == line.Leftx.maxlen):
-#        cum_left = np.array(line.Leftx)
-#        cum_right = np.array(line.Rightx)
         left_p = np.mean(np.array(line.Leftx),axis = 0)
         right_p = np.mean(np.array(line.Rightx),axis = 0)
-       
+     
+    # Queue not full, check if the last set of points are sane, and use them   
     elif (val):
-        left_p, right_p = pipe.fitCoeff(leftx, lefty,rightx,righty)
-    elif (line.last_leftx is not None):
-        left_p, right_p = pipe.fitCoeff(line.last_leftx, line.last_y,line.last_rightx, line.last_y)
-    else:
-        return imge
-        
-    left_fitx, right_fitx, ploty = pipe.fitVector(warped,left_p,right_p)
-    l,r = pipe.getRadiusCurve(np.array(left_fitx), np.array(right_fitx),ploty)
+        left_p, right_p = fitCoeff(leftx, lefty,rightx,righty)
     
+    # last set of points bad, use last sane  values, but first check if there are no sane points so far    
+    elif (line.last_leftx is not None):   # if 
+        left_p, right_p = fitCoeff(line.last_leftx, line.last_y,line.last_rightx, line.last_y)
+    
+    else:  # Total failure, skip this frame
+        return imge
+    
+    # use coeffiecints to get lane lines    
+    left_fitx, right_fitx, ploty = pipe.fitVector(warped,left_p,right_p)
+   
+    # get radii of left and right line
+    l,r = pipe.getRadiusCurve(np.array(left_fitx), np.array(right_fitx),ploty)
+   
+    # get offset from centre
     center_offset_m = pipe.getCameraOffset(imge,left_fitx, right_fitx)
     
-        
+    #get radius via avraging
     radius[1] = float("{0:.2f}".format((l+r)/2.0))
  
-
+    # unwarp the image to get the color image back
+    n_img = pipe.warpToColor(imge, warped,left_fitx, right_fitx, ploty)
+    
+    # if radius exceeds sanity, use last  sane value
     if (radius[1] > 3000):
         radius[1] = radius[0]
     string1 = "Radius of Curvature is = "+str(radius[1])+" (m)"
     string2 = "Camera off centre by = "+str(center_offset_m)+" (m)"
-    n_img = pipe.warpToColor(imge, warped,left_fitx, right_fitx, ploty)
-    if (indx//10  ):
+    
+    # Write radius and offset on every 10th frame
+    if (indx//10 ):
         cv2.putText(n_img,string1,(300,150), font, 1,(255,255,255),2)
         cv2.putText(n_img,string2,(300,250), font, 1,(255,255,255),2)
+  
     indx += 1
     radius[0] = radius[1]
     return n_img
 
 
+
+
+
+
 # Make an instance of the pipeline
 pipe = Pipeline_helper(NX,NY) 
-line_stat = Line_Stats()
+line = Line_Sanity()
+
 """ Collect all images for camera calibration"""
 
 images = pipe.collectImages(calib_image_dir)
 test_images = pipe.collectImages(test_image_dir)
 
-#import sys
-#sys.exit()
 import os
 if os.path.exists("Coeff.npy"):
     print("Using existing Coefficients ")
@@ -166,81 +169,17 @@ else:
     np.save('Mat.npy',mat)
 
 
+# remove distortion on test images
 output = []
-#test_image = images[10]
 for j in range(len(test_images)):
     output.append(pipe.cameraDistremove(test_images[j],mat,coeff,True))
 
 
-
-
-#for im in range(len(output)):
-#
-#    n_img,warped, left_fitx,right_fitx,ploty,img = pipeline(output[im])
-#   
-#    f, ((ax1, ax2,ax3)) = plt.subplots(1, 3, figsize=(24, 9))
-#    f.tight_layout()
-#    ax1.imshow(img)
-#    
-#    ax1.set_title('Sobel X')
-#    ax2.imshow(warped)
-#    ax2.plot(left_fitx, ploty, color='red')
-#    ax2.plot(right_fitx, ploty, color='red')
-#
-#
-#    ax3.imshow(n_img)
-#
-#    break
-#    
-
-
 white_output = 'output.mp4'
 
-clip2 = VideoFileClip('project_video.mp4')#.subclip(0,10)
+clip2 = VideoFileClip('project_video.mp4')#.subclip(0,2)
 yellow_clip = clip2.fl_image(pipeline)
 yellow_clip.write_videofile(white_output, audio=False)
-
-
-
-
-
-
-
-
-
-
-    
-    
-#    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(24, 9))
-#    f.tight_layout()
-#    ax1.imshow(a)
-#    ax1.set_title('Sobel X')
-#    ax2.imshow(b)
-#    ax2.set_title('Sobel Y')
-#    ax3.set_title('Mag threshold')
-#    ax4.set_title('Direction Threshold')
-#    ax3.imshow(c)
-#    ax4.imshow(d)
-#    plt.savefig('result_images/binary'+str(im)+'.jpg')
-#
-#    
-#    
-#    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(24, 9))
-#    f.tight_layout()
-#    ax1.imshow(imge)
-#    ax1.set_title('original')
-#    ax2.imshow(final)
-#    ax2.set_title('Binary Thresholds')
-#    ax3.set_title('Color Threshold')
-#    ax3.imshow(s_binary)
-#    ax4.set_title('Final')
-#    ax4.imshow(combined_binary)
-#    plt.savefig('result_images/Final'+str(im)+'.jpg')    
-#    
-
-
-
-
 
 
 
